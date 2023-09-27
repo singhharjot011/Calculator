210cf6a7639e968fa20f873e9daac2d1fca7962f
singhharjot011 <singhharjot011@gmail.com>
2023-09-27 00:27:31 -0400

    Improved Code without unwanted if statements and reduced HTML Code

--- script.js
+++ script.js
@@ -1,108 +1,127 @@
-let firstOperand = document.querySelector(".first-operand");
-let operator = document.querySelector(".operator")
-let secondOperand = document.querySelector(".second-operand");
+let operand1 = document.querySelector(".first-operand");
+let op = document.querySelector(".operator");
+let operand2 = document.querySelector(".second-operand");
 
+//Adding Buttons using JS instead of typing HTML for each button in numbers div
 
+let numsDiv = document.querySelector(".numbers");
+for (let i = 9; i >= 0; i--) {
+  let nums = document.createElement("button");
+  nums.className = "btn";
+  nums.id = "btn" + i;
+  nums.onclick = function () {
+    input(i);
+  };
+
+  let numsSpan = document.createElement("span");
+  numsSpan.className = "btn-content";
+  numsSpan.textContent = i;
+
+  nums.appendChild(numsSpan);
+  numsDiv.appendChild(nums);
+}
+
+// Adding . dot button after numbers separately
+let dot = document.createElement("button");
+dot.className = "btn";
+dot.id = "dot";
+dot.onclick = function () {
+  input(".");
+};
+
+let dotSpan = document.createElement("span");
+dotSpan.className = "btn-content";
+dotSpan.textContent = ".";
+
+dot.appendChild(dotSpan);
+numsDiv.appendChild(dot);
 
 //Taking Input from Keyboard
 addEventListener("keydown", (event) => {
-if(/[\d.%+*/-]/.test(event.key)){
-        input(event.key)
-    }else if(event.key==='Enter' && firstOperand.textContent !== '' && operator.textContent !=='' && secondOperand.textContent!==''){
-        equalTo();
-    }
-
+  if (/[\d.%+*/-]/.test(event.key)) {
+    input(event.key);
+  } else if (
+    event.key === "Enter" &&
+    operand1.textContent &&
+    op.textContent &&
+    operand2.textContent
+  ) {
+    equalTo();
+  }
 });
 
-
-//Taking input from all digits and operators
-function input(n){
-    
-    if(/[\d.]/.test(n) && operator.textContent == '' ){
-
-
-        
-        //If statement below make sure user can't enter . twice in same operand
-        if(n=='.' && !firstOperand.textContent.includes('.')){
-            firstOperand.textContent +=n;
-        }else if(/[\d]/.test(n)){
-            firstOperand.textContent +=n;
-        secondOperand.textContent='';
-        secondOperand.style.fontSize = '40px';
-        }
+//Taking input
+function input(inputChar) {
+  //only taking first operand when operator isn't present
+  if (!op.textContent && /^[\d.]+$/.test(inputChar)) {
+    operand2.textContent = ""; //Clear the result if present
+    //If statement below prevents second . in operand1
+    if (inputChar === "." && !operand1.textContent.includes(".")) {
+      operand1.textContent += inputChar;
+    } else if (inputChar !== ".") {
+      operand1.textContent += inputChar;
     }
-
-    //if statement below checks if input is operator, only enter 1 operator at a time
-    else if(/[+*/%-]/.test(n) && firstOperand.textContent !==''){
-    operator.textContent = n;
+  } else if (/[+*/%-]/.test(inputChar)) {
+    //if statement below allows to start new calculation on result
+    if (!operand2.textContent) {
+      op.textContent = inputChar;
+      //If statement below prevents re-entering operator if both operands are present
+    } else if (operand1.textContent) {
+      //Do nothing
+    } else {
+      operand1.textContent = operand2.textContent;
+      op.textContent = inputChar;
+      operand2.textContent = "";
     }
-
-    //If statement below limiting 1 . for second operand
-    else if (firstOperand.textContent !== '' && operator.textContent !=='') {
-            if(n=='.' && !secondOperand.textContent.includes('.')){
-                secondOperand.textContent +=n;
-            }
-            
-            //Taking digits for second operand
-            else if(/[\d]/.test(n)){
-                secondOperand.textContent += n;  
-            }
+  } else if (op.textContent && /^[\d.]+$/.test(inputChar)) {
+    //If statement below prevents second . in operand1
+    if (inputChar === "." && !operand2.textContent.includes(".")) {
+      operand2.textContent += inputChar;
+    } else if (inputChar !== ".") {
+      operand2.textContent += inputChar;
     }
-    
-    //Nested If statement below helps applying new calculation on existing result
-    else if(firstOperand.textContent=='' && operator.textContent =='' && secondOperand.textContent !==''){
-        if(/[+*/%-]/.test(n)
-        ){
-            firstOperand.textContent = secondOperand.textContent;
-            operator.textContent = n;
-            secondOperand.textContent = '';
-            secondOperand.style.fontSize = '40px';
-        }
-    }
+  }
+}
 
-};
-
-// AC Button Functionality
-function clearInput()  {
-    firstOperand.textContent = '';
-    operator.textContent = '';
-    secondOperand.textContent = '';
-};
-
+//AC Button Functionality
+function clearInput() {
+  operand1.textContent = "";
+  op.textContent = "";
+  operand2.textContent = "";
+}
 
 // +/- Button Functionality
-function abs(){
-    let re=/\-?\d+.+/;
-    if(firstOperand.textContent !=='' && operator.textContent == '' && secondOperand.textContent == ''){
-        let result = parseFloat(firstOperand.textContent) * -1;
-        firstOperand.textContent = result;
-    }else if(firstOperand.textContent !=='' && operator.textContent !== '' && secondOperand.textContent !== ''){
-        let result = parseFloat(secondOperand.textContent) * -1;
-        secondOperand.textContent = result;
-    }else if(firstOperand.textContent =='' && operator.textContent == '' && secondOperand.textContent !== ''){
-        let result = parseFloat(secondOperand.textContent) * -1;
-        secondOperand.textContent = result;
-    }
-};
+function abs() {
+  if (!op.textContent && operand1.textContent) {
+    let result = parseFloat(operand1.textContent) * -1;
+    operand1.textContent = result;
+  } else if (operand2.textContent) {
+    let result = parseFloat(operand2.textContent) * -1;
+    operand2.textContent = result;
+  }
+}
 
-
-
+// Percent Button Functionality
+function percent() {
+  if (!op.textContent && operand1.textContent) {
+    let result = parseFloat(operand1.textContent) / 100;
+    operand1.textContent = result;
+  } else if (operand2.textContent){
+    let result = parseFloat(operand2.textContent) / 100;
+    operand2.textContent = result;
+  }
+}
 
 //Equal To Button Functionality
-function equalTo(){
-    if(firstOperand.textContent !=='' && operator.textContent !== '' && secondOperand.textContent !== ''){
-        secondOperand.textContent = eval(firstOperand.textContent+operator.textContent+secondOperand.textContent);
-        firstOperand.textContent = '';
-        operator.textContent = ''; 
-        if(secondOperand.textContent.length<10){
-            secondOperand.style.fontSize = '80px';
-        }else{
-            secondOperand.style.fontSize = '40px';
-        }
-    }else{
+function equalTo() {
+  if (operand1.textContent && op.textContent && operand2.textContent) {
+    let result = eval(
 
-    }
-};
+      // Added space before and after operator to avoid -1--1 like situations
+      operand1.textContent + " " + op.textContent + " "+ operand2.textContent
+    );
 
-
-
+    clearInput();
+    operand2.textContent = result;
+  }
+}
